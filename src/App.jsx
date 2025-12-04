58401b5fee1e69a61606af5b44a7114eff44af78
Mannat Mehta <151730567+mannatmehta05@users.noreply.github.com>
2025-12-04 22:28:08 +0530

    worked on toasters
    worked on toasters

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { RouterProvider } from "react-router";
 import { routes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./context/auth/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={routes} />
-    </AuthProvider>
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <AuthProvider>
+        <RouterProvider router={routes} />
+      </AuthProvider>
+    </>
   );
 }
 export default App;
