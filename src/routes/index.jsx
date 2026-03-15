7236f53dc946976988cc25aadf65629a6f04a047
amrit90413 <82268419+amrit90413@users.noreply.github.com>
2026-03-15 17:54:45 +0530

    feat: sync visuals with mannat smart-zone repo

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import LandingPage from "../pages/landingPage";
 import TermsOfServices from "../pages/termsOfServices";
 import PrivacyPolicy from "../pages/privacyPolicy";
+import ContactUs from "../pages/conatctUs";
+import FaqPage from "../pages/faqPage";
+
 import DeleteAccount from "../pages/deleteAccount";
 import SignIn from "../pages/auth/signIn";
 import SignUp from "../pages/auth/signUp";
@@ -46,6 +49,14 @@ export const routes = createBrowserRouter([
   {
     path: "/privacy-policy",
     element: <PrivacyPolicy />,
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs />,
+  },
+  {
+    path: "/faq",
+    element: <FaqPage />,
   },
   {
     path:"/subscribe",
